c2730128b54c518e21ba67b5cceee5f3d7880186
Alexey Andreev <bigalex03@mail.ru>
2024-09-17 16:24:03 +0300

    Перемещение в общую папку

--- Lab1/main.py
+++ Lab1/main.py
@@ -3,112 +3,88 @@
 import cv2
 
 ############# Задача 2 ####################
+# import cv2
+#
+# folder = 'C:/Users/bigal/PycharmProjects/ATSOM/Lab1/Test/'
+#
+# img1 = cv2.imread(folder + 'Cat1.jpg', cv2.IMREAD_UNCHANGED)
+# img2 = cv2.imread(folder + 'Cat2.png', cv2.IMREAD_ANYDEPTH)
+# img3 = cv2.imread(folder + 'Dog1.bmp', cv2.IMREAD_GRAYSCALE)
+#
+# cv2.namedWindow('Cat1', cv2.WINDOW_NORMAL)
+# cv2.namedWindow('Cat2', cv2.WINDOW_AUTOSIZE)
+# cv2.namedWindow('Dog1', cv2.WINDOW_FULLSCREEN)
+#
+# cv2.imshow('Cat1', img1)
+# cv2.imshow('Cat2', img2)
+# cv2.imshow('Dog1', img3)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-# Расширения 3 шт
-# extensions = ['.jpg', '.png', '.bmp']
-#
-# # Создания окна 3 шт
-# window_flags = [cv2.WINDOW_NORMAL, cv2.WINDOW_AUTOSIZE, cv2.WINDOW_FULLSCREEN]
-#
-# # Чтение изображения 3 шт
-# read_flags = [cv2.IMREAD_COLOR, cv2.IMREAD_GRAYSCALE, cv2.IMREAD_UNCHANGED]
-#
-# image_path = r'Test/Screenshot_1.png'
-#
-# for ext in extensions:
-#     for w_flag in window_flags:
-#         for r_flag in read_flags:
-#
-#             image = cv2.imread(image_path, r_flag)
-#
-#
-#             window_name = f'Image ({ext}, {w_flag}, {r_flag})'
-#             cv2.namedWindow(window_name, w_flag)
-#
-#
-#             cv2.imshow(window_name, image)
-#
-#
-#             cv2.waitKey(0)
-#
-#
-#             cv2.destroyWindow(window_name)
 
 ############# Задача 3 ####################
 
-# Путь
-# video_path = r'C:\Users\bigal\PycharmProjects\ATSOM\Lab1\Test\video.mp4'
+# # Путь
+# folder = 'C:/Users/bigal/PycharmProjects/ATSOM/Lab1/Test/'
+#
+# # Создаем объект VideoCapture для чтения видео из файла
+# cap = cv2.VideoCapture(folder + 'video.mp4', cv2.CAP_ANY)
 #
 #
-# cap = cv2.VideoCapture(video_path)
+# # Получаем параметры видео
+# fps = cap.get(cv2.CAP_PROP_FPS)  # Частота кадров
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Ширина кадров
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # Высота кадров
 #
-# # Проверка
-# if not cap.isOpened():
-#     print("Не удалось открыть видео!")
-#     exit()
-#
-# # Получение параметров видео
-# fps = cap.get(cv2.CAP_PROP_FPS)
-# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-#
-#
+# # Определяем форматы для отображения видео
 # formats = [
-#     (width, height, cv2.COLOR_BGR2RGB),
-#     (int(width / 2), int(height / 2), cv2.COLOR_BGR2GRAY),
-#     (int(width * 2), int(height * 2), cv2.COLOR_BGR2HSV)
+#     (width, height, cv2.COLOR_BGR2RGB),  # Оригинальный размер, цветовая гамма RGB
+#     (int(width / 2), int(height / 2), cv2.COLOR_BGR2GRAY),  # Уменьшенный размер, оттенки серого
+#     (int(width * 2), int(height * 2), cv2.COLOR_BGR2HSV)  # Увеличенный размер, цветовая гамма HSV
 # ]
 #
-# # Отображение видео
-# for new_width, new_height, color_conv in formats:
-#     # Создание окна
-#     window_name = f'Video ({new_width}x{new_height})'
+#
+# for new_width, new_height, color_conv in formats: # Отображаем видео в различных форматах
+#     window_name = f'Video ({new_width}x{new_height})' # Создаем окно с соответствующим названием
 #     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 #
 #     while True:
-#         # Чтение кадра
+#         # Читаем очередной кадр видео
 #         ret, frame = cap.read()
 #         if not ret:
 #             break
 #
-#         # Изменение размера и цветов
+#         # Изменяем размер и цветовую гамму кадра
 #         frame = cv2.resize(frame, (new_width, new_height))
 #         frame = cv2.cvtColor(frame, color_conv)
 #
-#
 #         cv2.imshow(window_name, frame)
 #
-#
-#         key = cv2.waitKey(int(1000 / fps)) & 0xFF
-#         if key == ord('q'):
+#         if cv2.waitKey(int(1000 / fps)) & 0xFF == 27:
 #             break
 #
-#
+#     # Закрываем окно
 #     cv2.destroyWindow(window_name)
-#
 #
 # cap.release()
 # cv2.destroyAllWindows()
 
 ############# Задача 4 ####################
 
-# # Путь к исходному видео
-# input_video_path = r'C:\Users\bigal\PycharmProjects\ATSOM\Lab1\Test\video.mp4'
+# # Путь
+# folder = 'C:/Users/bigal/PycharmProjects/ATSOM/Lab1/Test/'
 #
-# # Путь к выходному видео
-# output_video_path = r'C:\Users\bigal\PycharmProjects\ATSOM\Lab1\Test\output_video.avi'
+# # Создаем объект VideoCapture для чтения видео из файла
+# cap = cv2.VideoCapture(folder + 'video.mp4')
 #
-#
-# cap = cv2.VideoCapture(input_video_path)
-#
-#
-# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# fps = cap.get(cv2.CAP_PROP_FPS)
+# # Получаем параметры видео
+# fps = cap.get(cv2.CAP_PROP_FPS)  # Частота кадров
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Ширина кадров
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # Высота кадров
 #
 # #Запись выходного видео
 # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+# out = cv2.VideoWriter(folder + 'output_video.avi', fourcc, fps, (width, height))
 #
 # # Копирование кадров
 # while True:
@@ -121,127 +97,140 @@ import cv2
 # cap.release()
 # out.release()
 
-############# Задача 5 ####################
+############ Задача 5 ####################
 
 
 # # Путь
-# image_path = r'C:\Users\bigal\PycharmProjects\ATSOM\Lab1\Test\Screenshot_1.png'
+# folder = 'C:/Users/bigal/PycharmProjects/ATSOM/Lab1/Test/'
 #
-
-# image = cv2.imread(image_path)
+#
+# image = cv2.imread(folder + 'Cat1.jpg')
 #
 # # Перевод изображения в формат HSV
 # hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 #
-
+#
 # cv2.namedWindow('Original Image', cv2.WINDOW_NORMAL)
 # cv2.namedWindow('HSV Image', cv2.WINDOW_NORMAL)
 #
-
+#
 # cv2.imshow('Original Image', image)
 # cv2.imshow('HSV Image', hsv_image)
 #
 # cv2.waitKey(0)
 #
 # cv2.destroyAllWindows()
-
-
-############# Задача 6 ####################
-
 #
-# # Открытие камеры
+
+# ############ Задача 6 ####################
+#
+
+# import cv2
+# import numpy as np
+#
 # cap = cv2.VideoCapture(0)
 #
+# # Получение размеров кадра
+# frame_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Ширина
+# frame_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # Высота
+#
+# # Определение прямоугольных областей
+# rectangles = np.array([  # значения [x1, y1], [x2, y2]
+#     [[  0, 140], [260, 180]],
+#     [[110,   0], [150, 140]],
+#     [[110, 180], [150, 320]]
+# ])
+#
+# offset_x = frame_w // 2 - rectangles[:, :, 0].max() // 2  # Горизонтальное смещение прямоугольников
+# offset_y = frame_h // 2 - rectangles[:, :, 1].max() // 2  # Вертикальное смещение прямоугольников
+#
 # while True:
-
 #     ret, frame = cap.read()
 #
-
-#     height, width, _ = frame.shape
+#     height, width, _ = frame.shape  # Получение размеров кадра
 #
 #     # Вычисление координат центра кадра
 #     center_x = width // 2
 #     center_y = height // 2
 #
-#     # Рисование красного креста в центре кадра
-#     # Горизонтальная линия
-#     cv2.rectangle(frame, (center_x - 100, center_y - 10), (center_x + 100, center_y + 10), (0, 0, 255), 0)
-#     # Вертикальная линия
-#     cv2.rectangle(frame, (center_x - 10, center_y - 100), (center_x + 10, center_y + 100), (0, 0, 255), 0)
+#     # Применение размытия к определенной области
+#     x1, y1 = rectangles[0][0]
+#     x2, y2 = rectangles[0][1]
+#     mask = np.zeros((frame_h, frame_w, 3), dtype=np.uint8)
+#     mask = cv2.rectangle(mask, (x1 + offset_x, y1 + offset_y), (x2 + offset_x, y2 + offset_y), (255, 255, 255), -1)
+#     blur = cv2.stackBlur(frame, (63, 63)) # Размытие
+#     frame[mask == 255] = blur[mask == 255]
 #
-
-#     cv2.imshow('Camera', frame)
+#     # Рисование прямоугольных областей
+#     for rect in rectangles:
+#         x1, y1 = rect[0]
+#         x2, y2 = rect[1]
+#         cv2.rectangle(frame, (x1 + offset_x, y1 + offset_y), (x2 + offset_x, y2 + offset_y), (255, 0, 0), 2)
 #
-
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#     cv2.imshow("Red cross", frame)
+#
+#     if cv2.waitKey(1) & 0xFF == 27:
 #         break
 #
-
 # cap.release()
 # cv2.destroyAllWindows()
+
 
 ############# Задача 7 ####################
 
 # import os
 #
-#
-# output_dir = r'C:\Users\bigal\PycharmProjects\ATSOM\Lab1\Test'
-# if not os.path.exists(output_dir):
-#     os.makedirs(output_dir)
-#
+# folder = 'C:/Users/bigal/PycharmProjects/ATSOM/Lab1/Test'
+# if not os.path.exists(folder):  # Проверка, существует ли папка
+#     os.makedirs(folder)
 #
 # cap = cv2.VideoCapture(0)
 #
-#
-# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Ширина
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # Высота
 # fps = cap.get(cv2.CAP_PROP_FPS)
 #
 # # Запись видео
-# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-# out = cv2.VideoWriter(os.path.join(output_dir, 'output_video.mp4'), fourcc, fps, (width, height))
+# fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Кодек для записи видео в MP4
+# out = cv2.VideoWriter(os.path.join(folder, 'output_video.mp4'), fourcc, fps, (width, height))
 #
 # while True:
-#
 #     ret, frame = cap.read()
 #
-#     # Запись кадра в файл
-#     out.write(frame)
 #
+#     out.write(frame)  # Запись кадра в выходной видеофайл
 #
 #     cv2.imshow('Camera', frame)
 #
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#     if cv2.waitKey(1) & 0xFF == 27:
 #         break
 #
-# # Освобождение ресурсов
+#
 # cap.release()
 # out.release()
 # cv2.destroyAllWindows()
 #
 # # Демонстрация записанного видео
-# cap = cv2.VideoCapture(os.path.join(output_dir, 'output_video.mp4'))
+# cap = cv2.VideoCapture(os.path.join(folder, 'output_video.mp4'))  # Открытие записанного видео
 #
-# while True:
+# while True:  # Основной цикл воспроизведения видео
 #     # Считывание кадра с видео
 #     ret, frame = cap.read()
 #
-#     if not ret:
+#     if not ret:  # Проверка, есть ли кадры для отображения
 #         break
 #
-#     cv2.imshow('Recorded Video', frame)
+#     cv2.imshow('Recorded Video', frame)  # Отображение кадра
 #
 #     # Уменьшение скорости воспроизведения
-#     cv2.waitKey(10)
+#     cv2.waitKey(10)  # Задержка для замедления воспроизведения
 #
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#     if cv2.waitKey(1) & 0xFF == 27:  # Выход по нажатию клавиши 'Esc'
 #         break
 #
-# # Освобождение ресурсов
-# cap.release()
-# cv2.destroyAllWindows()
+# cap.release()  # Освобождение ресурсов видео
+# cv2.destroyAllWindows()  # Закрытие всех окон OpenCV
+
 
 
 ############# Задача 8 ####################
@@ -251,7 +240,7 @@ import cv2
 # cap = cv2.VideoCapture(0)
 #
 # while True:
-
+#
 #     ret, frame = cap.read()
 #
 #     # Получение размеров кадра
@@ -281,14 +270,14 @@ import cv2
 #     # Вертикальная линия
 #     cv2.rectangle(frame, (center_x - 10, center_y - 100), (center_x + 10, center_y + 100), color, -1)
 #
-
+#
 #     cv2.imshow('Camera', frame)
 #
-
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#
+#     if cv2.waitKey(1) & 0xFF == 277:
 #         break
 #
-
+#
 # cap.release()
 # cv2.destroyAllWindows()
 
